1fb35e345ba4cc8a3b647369c999ac0695398b9f
alexc <alexc@DESKTOP-KENN2ND>
2017-02-22 00:53:01 +0100

    LAUNCHER +-

--- Tales of The Warrior/LAUNCHER.Designer.cs
+++ Tales of The Warrior/LAUNCHER.Designer.cs
@@ -30,8 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LAUNCHER));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_exit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -47,21 +48,24 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // btn_play
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(925, 103);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 56);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseMnemonic = false;
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_play.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_play.FlatAppearance.BorderSize = 0;
+            this.btn_play.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_play.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_play.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.ForeColor = System.Drawing.Color.Aqua;
+            this.btn_play.Location = new System.Drawing.Point(931, 336);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(193, 56);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "PLAY ";
+            this.btn_play.UseMnemonic = false;
+            this.btn_play.UseVisualStyleBackColor = false;
+            this.btn_play.MouseEnter += new System.EventHandler(this.SelMouseEnter);
+            this.btn_play.MouseLeave += new System.EventHandler(this.SelMouseLeave);
             // 
             // pictureBox2
             // 
@@ -73,14 +77,34 @@
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.Aqua;
+            this.btn_exit.Location = new System.Drawing.Point(931, 497);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(193, 56);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "EXIT ";
+            this.btn_exit.UseMnemonic = false;
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.MouseEnter += new System.EventHandler(this.SelMouseEnter);
+            this.btn_exit.MouseLeave += new System.EventHandler(this.SelMouseLeave);
+            // 
             // LAUNCHER
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 636);
             this.ControlBox = false;
+            this.Controls.Add(this.btn_exit);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_play);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.Transparent;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -96,7 +120,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_play;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_exit;
     }
 }
--- Tales of The Warrior/LAUNCHER.cs
+++ Tales of The Warrior/LAUNCHER.cs
@@ -8,10 +8,24 @@ namespace Tales_of_The_Warrior
         public LAUNCHER()
         {
             InitializeComponent();
-            button1.Parent = pictureBox1;
-            button1.BackColor = Color.Transparent;
+            btn_play.Parent = pictureBox1;
+            btn_play.BackColor = Color.Transparent;
+            btn_exit.Parent = pictureBox1;
+            btn_exit.BackColor = Color.Transparent;
             pictureBox2.Parent = pictureBox1;
             pictureBox2.BackColor = Color.Transparent;
+        }
+
+        private void SelMouseEnter(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Text += "{";
+        }
+
+        private void SelMouseLeave(object sender, System.EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Text = b.Text.TrimEnd('{');
         }
     }
 }
